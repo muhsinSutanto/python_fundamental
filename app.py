43901f2dc22fcd5e6942251253450c30bbe1d7ac
muhsin sutanto <muhsin@femaledaily.com>
2019-05-20 15:14:31 +0700

    basic phyton

---
+++ app.py
@@ -0,0 +1,93 @@
+# print('muhsin sutanto')
+# print('o----')
+# print('  ||||')
+# print('*' * 10 )
+
+# price = 10
+# price = 20
+# rating = 4.9
+# print(price)
+
+# name = input('what is yout name?')
+# favourite_color = input('what is favourite color')
+# print(name + favourite_color)
+
+# birth_year = input('Birth year: ')
+# print((type(birth_year)))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# course = '''
+# hi there
+#
+# this is
+#
+# how  string on python is different
+# '''
+#
+# print(course)
+
+
+# course = 'python string character'
+# print(course[:5])
+
+# first = 'Jhon'
+# second = 'Smith'
+# message = f'{first} [{second}] is coder'
+# print(message)
+
+# string method read for more info
+
+# course = 'Phyton for begginer'
+# print(course.replace('begginer', 'Begginer'))
+
+#augmented assignment oprator
+# x = 10
+# x += 3
+# print(x)
+
+# x = 2 ** 3
+# print(x)
+
+#number method
+# x = 2.9
+# print(round(x))
+# print(abs(x))
+
+#read math documentation
+# import math
+#
+# x = 2.3
+# print(math.ceil(x))
+
+# is_hot = False
+# is_cold = True
+#
+# if is_hot:
+#     print('its a hot day')
+# elif is_cold:
+#     print('its cold day')
+#     print('drink cold water')
+# else:
+#     print("it's lovely day")
+
+
+# has_high_income = False
+# has_good_credit = True
+#
+# if has_high_income or has_good_credit:
+#     print('eligible for loan')
+
+
+# temperature = 30
+#
+# if temperature > 30:
+#     print('it is hot day')
+# else:
+#     print('it is not a hod day')
+
+
+
+
+
